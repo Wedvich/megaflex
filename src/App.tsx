0a38e1bb115af5a2f02797af8b9263c86eb02614
Martin Wedvich <wedvich@gmail.com>
2019-09-30 20:37:53 +0200

    Add drag handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { hot } from 'react-hot-loader/root';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 
 import './app.scss';
 import './app2.scss';
@@ -8,9 +8,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Exercise, RootState } from './store/rootReducer';
 import { pluralize } from './common/utils';
 
+const ExerciseListItemDragHandle = SortableHandle(() => <span className="exercise-list-item__drag-handle">☰</span>);
+
 const ExerciseListItem = SortableElement(({ value, prefix }: any) => {
   return (
     <li className="exercise-list-item">
+      <ExerciseListItemDragHandle />
       <div>
         <div className="exercise-list-item__label">
           <span className="exercise-list-item__prefix">{prefix}</span>
@@ -55,7 +58,14 @@ const App = () => {
 
   return (
     <>
-      <ExerciseList items={exercises} onSortEnd={onSortEnd} helperClass="exercise-list-item dragging" lockAxis="y" />
+      <ExerciseList
+        items={exercises}
+        onSortEnd={onSortEnd}
+        helperClass="exercise-list-item dragging"
+        lockAxis="y"
+        useDragHandle
+        useWindowAsScrollContainer
+      />
       <button className="reset-button" onClick={onResetClicked}>
         Reset
       </button>
